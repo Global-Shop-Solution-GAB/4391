5c2aea36454f9e73fc2b30d9f5b98c0bc3426d57
Service Web <sw@gssmail.com>
2024-03-11 16:28:29 -0500

    Commit changes in C:\Websites\Git\emarlow\4391\GCG_4391_BIDQO.sql

---
+++ GCG_4391_BIDQO.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "GCG_4391_BIDQO"(
+ "SO" CHAR(9) NOT NULL ,
+ "SO_Ln" CHAR(4) NOT NULL ,
+ "Create_Calendar" DATE,
+ "SendInvite_OSS" DATE,
+ "Prepare_EFile" DATE,
+ "DevelopScope_Bid" DATE,
+ "SendScope_Contractor" DATE,
+ "BidTeam_Analyze" DATE,
+ "AssignProject_ISS" DATE,
+ "Quote_Due" DATE,
+ "Quote_Given" DATE,
+ PRIMARY KEY ("SO", "SO_Ln"));
